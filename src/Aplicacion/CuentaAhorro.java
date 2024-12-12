50d928d2585c240563c894d3f75bbf237de4c438
Alejandra Contreras <user.email>
2024-12-12 20:45:44 -0300

    Primer commit

---
+++ src/Aplicacion/CuentaAhorro.java
@@ -0,0 +1,28 @@
+package Aplicacion;
+
+public class CuentaAhorro extends Cuenta{
+
+	  private double tasaDeIntereses;
+
+	  public void calcularIntereses() {
+	    double intereses= this.getSaldo() * tasaDeIntereses;
+	    System.out.println("Intereses actuales : " + intereses);
+	  }
+
+	  public void retirar(double valor) {
+	    double tasaDeRetiro= 0.01;
+	    super.retirar(valor + tasaDeRetiro);
+	  }
+
+	  //getters y setters
+	  
+	public double getTasaDeIntereses() {
+		return tasaDeIntereses;
+	}
+
+	public void setTasaDeIntereses(double tasaDeIntereses) {
+		this.tasaDeIntereses = tasaDeIntereses;
+	}
+
+	  
+	}
